f4ddaa60958997d9f634d826cfb6707cf78c98bd
Peter Retzlaff <pe.retzlaff@gmail.com>
2020-03-22 13:23:26 +0100

    Trash-SVG looked trashy in Chrome, so just go with button text for now...

--- src/Employee.js
+++ src/Employee.js
@@ -126,9 +126,9 @@ function Employee({ index, removable }) {
       </td>
 
       {removable && <td className="p-2 border">
-        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded inline-flex items-center"
                 onClick={() => dispatch(removeEmployee(index))}>
-          <img src={trash} height="20px" width="20px"></img>
+          Remove
         </button>
       </td>}
     </>
