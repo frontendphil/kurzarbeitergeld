b4a92c83c5e92ee5dab2d0ad2c65226076d2c431
Daniel Moritz <daniel.moritz@nexenio.com>
2020-03-22 15:54:42 +0100

    dynamically use employees request data

--- api/createKugEmployeesList.py
+++ api/createKugEmployeesList.py
@@ -58,28 +58,31 @@ class handler(BaseHTTPRequestHandler):
         #     }
         # }        
 
-
         data_dict = {
             'Die 9-stellige Stamm-Nr. Kug besteht\r1\) aus dem vorbelegten Buchstaben K und 2\) aus dem 8-stelligen Ausfüllfeld. Bitte Stamm-Nr. Kug eingeben': '111223344', # Stamm-Nr. Kug
             'Ableitungs-Nr. vierstellig': '1234', # Ableitungs-Nr
-            'Abrechnungsmonat': 'März',
-            'Reihe 3 Spalte 1:': '1',
-            'Reihe 3 Spalte 2: Name, Vorname': 'Mustermann, Max',
-            'Reihe 3 Spalte 2: Versicherungsnummer 12-stellig': 'VD-323850234923',
-            'Reihe 3 Spalte 2: Faktor 0,': '21',
-            'Reihe 3 Spalte 2: Geben Sie die restlichen drei Ziffern bezüglich des Faktors ein': '123',
-            'Reihe 3 Spalte 3: Umfang des Arbeitsausfalls: Anzahl der Kug-Ausfallstunden': '30',
-            'Reihe 3 Spalte 3: Umfang des Arbeitsausfalls: Anzahl der Krankengeldstunden': '0',
-            # 'Reihe 3 Spalte 3: Umfang des Arbeitsausfalls: Hier werden die Kug-Ausfallstunden und die Krankengeldstunden automatisch summiert': '30',
-            'Reihe 3 Spalte 4: Soll-Entgelt \(ungerundet\)': ' 2.500,00',
-            'Reihe 3 Spalte 5: Ist-Entgelt \(ungerundet\)': '1.250,00',
-            'Reihe 3 Spalte 6: Lohnsteuerklasse': 'III',
-            'Reihe 3 Spalte 6: Leistungssatz 1 oder 2': '1',
-            'Reihe 3 Spalte 7: Rechnerischer Leistungssatz für das Soll-Entgelt \(Spalte 4\) lt. Tabelle': '1.295,11',
-            'Reihe 3 Spalte 8: Rechnerischer Leistungssatz für das Ist-Entgelt \(Spalte 5\) lt. Tabelle': '675,36',
-            'Reihe 3 Spalte 9: Durchschnittliche Leistung pro Stunde \(Spalte 7 ./. Spalte 8: Insgesamtstunden aus Spalte 3\)': '150',
-            'Reihe 3 Spalte 10: Auszuzahlendes Kug \(Spalte 7 ./. Spalte 8\) oder Kug-Stunden Spalte 3 x durchschnittliche Leistung \(Spalte 9\)': '619,75'
+            'Abrechnungsmonat:': 'März'
         }
+
+        for employeeNumber, employee in enumerate(requestBody["employees"], start=1):
+            rowNumber = employeeNumber + 2
+            data_dict[f"Reihe {rowNumber} Spalte 1: Hier wird automatisch die laufende Nummer eingetragen"] = employeeNumber
+            data_dict[f"Reihe {rowNumber} Spalte 2: Name, Vorname"] = employee["name"]
+            data_dict[f"Reihe {rowNumber} Spalte 2: Versicherungsnummer 12-stellig"] = employee["insuranceNumber"]
+            # data_dict[f"Reihe {rowNumber} Spalte 2: Faktor 0,"] = employeeNumber[""]
+            # data_dict[f"Reihe {rowNumber} Spalte 2: Geben Sie die restlichen drei Ziffern bezüglich des Faktors ein"] = employeeNumber[""]
+            data_dict[f"Reihe {rowNumber} Spalte 3: Umfang des Arbeitsausfalls: Anzahl der Kug-Ausfallstunden"] = employee["lostHours"]
+            # data_dict[f"Reihe {rowNumber} Spalte 3: Umfang des Arbeitsausfalls: Anzahl der Krankengeldstunden"] = "0"
+            data_dict[f"Reihe {rowNumber} Spalte 3: Umfang des Arbeitsausfalls: Hier werden die Kug-Ausfallstunden und die Krankengeldstunden automatisch summiert"] = employee["lostHours"]
+            data_dict[f"Reihe {rowNumber} Spalte 4: Soll-Entgelt \(ungerundet\)"] = employee["regularSalaryBeforeTax"]
+            data_dict[f"Reihe {rowNumber} Spalte 5: Ist-Entgelt \(ungerundet\)"] = employee["currentSalaryBeforeTax"]
+            data_dict[f"Reihe {rowNumber} Spalte 6: Lohnsteuerklasse"] = employee["taxClass"]
+            # data_dict[f"Reihe {rowNumber} Spalte 6: Leistungssatz 1 oder 2"] = employee[""]
+            data_dict[f"Reihe {rowNumber} Spalte 7: Rechnerischer Leistungssatz für das Soll-Entgelt \(Spalte 4\) lt. Tabelle"] = employee["regularSalaryAfterTax"]
+            data_dict[f"Reihe {rowNumber} Spalte 8: Rechnerischer Leistungssatz für das Ist-Entgelt \(Spalte 5\) lt. Tabelle"] = employee["currentSalaryAfterTax"]
+            # data_dict[f"Reihe {rowNumber} Spalte 9: Durchschnittliche Leistung pro Stunde \(Spalte 7 ./. Spalte 8: Insgesamtstunden aus Spalte 3\)"] = employee[""]
+            # data_dict[f"Reihe {rowNumber} Spalte 10: Auszuzahlendes Kug \(Spalte 7 ./. Spalte 8\) oder Kug-Stunden Spalte 3 x durchschnittliche Leistung \(Spalte 9\)"] = employee[""]
+            
 
         template_pdf = pdfrw.PdfReader(TEMPLATE_PATH)
 
